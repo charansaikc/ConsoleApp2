28eb6920d3c2971f3d8f344a81dfc9083b7aaf69
Akhilesh <mailto:tpvakhilesh@gmail.com>
2023-10-10 15:04:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public delegate string AttachStringToEachOther(string s1, string s2);
+
+class A
+{
+    public static string ConcatStrings(string s1, string s2)
+    {
+        return s1 + s2;
+    }
+
+    static void Main()
+    {
+        AttachStringToEachOther attachDelegate = ConcatStrings;
+
+        string result = attachDelegate("Hello, ", "world!");
+
+        Console.WriteLine("Concatenated String: " + result);
+    }
+}
